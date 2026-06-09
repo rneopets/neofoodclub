29168bd63a128af8143bf42fe8146a61ae9eb6eb
Steve C <diceroll123@gmail.com>
2026-06-09 01:54:27 -0400

    spacing tweaks

--- src/app/components/tables/Td.tsx
+++ src/app/components/tables/Td.tsx
@@ -3,9 +3,7 @@ import { Table } from '@chakra-ui/react';
 // this element is a chakra <Td> but with less y-padding, to make our tables a little less large
 
 const Td = (props: React.ComponentProps<typeof Table.Cell>): React.ReactElement => (
-  <Table.Cell py={1} {...props}>
-    {props.children}
-  </Table.Cell>
+  <Table.Cell {...props}>{props.children}</Table.Cell>
 );
 
 export default Td;
