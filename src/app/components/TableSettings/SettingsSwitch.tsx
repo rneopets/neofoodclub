92c684663227d85528becb5e449b3ad5ca8ce339
Steve C <diceroll123@gmail.com>
2026-09-06 02:52:37 -0400

    Revert "Add "New" indicator to Sticky Place Bet Buttons setting (#1050)"
    This reverts commit fdae268d1710b46cb28d04bc6c8f65f71d3e4f47.

--- src/app/components/TableSettings/SettingsSwitch.tsx
+++ src/app/components/TableSettings/SettingsSwitch.tsx
@@ -1,10 +1,10 @@
-import React, { ChangeEvent, MouseEvent, ReactNode } from 'react';
+import React, { ChangeEvent, MouseEvent } from 'react';
 
 import SettingsRow from './SettingsRow';
 
 interface SwitchBoxProps {
   icon: React.ElementType;
-  label: ReactNode;
+  label: string;
   colorPalette: string;
   checked: boolean;
   onChange: (e: ChangeEvent<HTMLInputElement> | MouseEvent<HTMLDivElement>) => void;
